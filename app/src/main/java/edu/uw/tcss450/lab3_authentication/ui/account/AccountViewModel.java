b81d4f9daae901801b82a67013ac57502c507114
Katlyn <cup_kate@hotmail.com>
2021-11-04 02:01:23 -0700

    Refactoring the project to get rid of Lab 3 stuff.

--- app/src/main/java/edu/uw/tcss450/lab3_authentication/ui/account/AccountViewModel.java
+++
@@ -1,6 +0,0 @@
-package edu.uw.tcss450.lab3_authentication.ui.account;
-
-import androidx.lifecycle.ViewModel;
-
-public class AccountViewModel extends ViewModel {
-}
--- app/src/main/java/edu/uw/tcss450/lab3_authentication/ui/weather/WeatherViewModel.java
+++
@@ -1,6 +0,0 @@
-package edu.uw.tcss450.lab3_authentication.ui.weather;
-
-import androidx.lifecycle.ViewModel;
-
-public class WeatherViewModel extends ViewModel {
-}
