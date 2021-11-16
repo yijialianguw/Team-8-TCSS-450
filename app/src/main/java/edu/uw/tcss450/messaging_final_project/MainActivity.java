b7f31f5b320aace8140fdfd7aabc148ffecaf961
JeffSchultz99 <jeffschultz99@gmail.com>
2021-11-15 20:18:53 -0800

    lab 5 messaging changes
    I changed a ton of stuff in this commit. not ready to be merged. need to do some testing and make sure i didnt screw anything up

--- app/src/main/java/edu/uw/tcss450/messaging_final_project/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/messaging_final_project/MainActivity.java
@@ -3,22 +3,37 @@ package edu.uw.tcss450.messaging_final_project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import edu.uw.tcss450.messaging_final_project.databinding.ActivityMainBinding;
+import edu.uw.tcss450.messaging_final_project.model.NewMessageCountViewModel;
 import edu.uw.tcss450.messaging_final_project.model.UserInfoViewModel;
+import edu.uw.tcss450.messaging_final_project.services.PushReceiver;
+import edu.uw.tcss450.messaging_final_project.ui.chat.ChatMessage;
+import edu.uw.tcss450.messaging_final_project.ui.chat.ChatViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private MainPushMessageReceiver mPushMessageReceiver;
+    private ActivityMainBinding binding;
+    private NewMessageCountViewModel mNewMessageModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,30 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        mNewMessageModel = new ViewModelProvider(this).get(NewMessageCountViewModel.class);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.navigation_chat) {
+                //When the user navigates to the chats page, reset the new message count.
+                //This will need some extra logic for your project as it should have
+                //multiple chat rooms.
+                mNewMessageModel.reset();
+            }
+        });
+
+        mNewMessageModel.addMessageCountObserver(this, count -> {
+            BadgeDrawable badge = binding.navView.getOrCreateBadge(R.id.navigation_chat);
+            badge.setMaxCharacterCount(2);
+            if (count > 0) {
+                //new messages! update and show the notification badge.
+                badge.setNumber(count);
+                badge.setVisible(true);
+            } else {
+                //user did some action to clear the new messages, remove the badge
+                badge.clearNumber();
+                badge.setVisible(false);
+            }
+        });
 
     }
 
@@ -60,5 +99,55 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mPushMessageReceiver == null) {
+            mPushMessageReceiver = new MainPushMessageReceiver();
+        }
+        IntentFilter iFilter = new IntentFilter(PushReceiver.RECEIVED_NEW_MESSAGE);
+        registerReceiver(mPushMessageReceiver, iFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mPushMessageReceiver != null){
+            unregisterReceiver(mPushMessageReceiver);
+        }
+    }
+
+    /**
+     * A BroadcastReceiver that listens for messages sent from PushReceiver
+     */
+    private class MainPushMessageReceiver extends BroadcastReceiver {
+
+        private ChatViewModel mModel =
+                new ViewModelProvider(MainActivity.this)
+                        .get(ChatViewModel.class);
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            NavController nc =
+                    Navigation.findNavController(
+                            MainActivity.this, R.id.nav_host_fragment);
+            NavDestination nd = nc.getCurrentDestination();
+
+            if (intent.hasExtra("chatMessage")) {
+
+                ChatMessage cm = (ChatMessage) intent.getSerializableExtra("chatMessage");
+
+                //If the user is not on the chat screen, update the
+                // NewMessageCountView Model
+                if (nd.getId() != R.id.navigation_chat) {
+                    mNewMessageModel.increment();
+                }
+                //Inform the view model holding chatroom messages of the new
+                //message.
+                mModel.addMessage(intent.getIntExtra("chatid", -1), cm);
+            }
+        }
     }
 }
