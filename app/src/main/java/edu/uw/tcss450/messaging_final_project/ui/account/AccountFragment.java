fd8cc30e677b086b00a9310e9d1ff17092d61f01
Artem Potafiy <apotafiy@uw.edu>
2021-11-25 00:34:09 -0800

    sign out functionality

--- app/src/main/java/edu/uw/tcss450/messaging_final_project/ui/account/AccountFragment.java
+++ app/src/main/java/edu/uw/tcss450/messaging_final_project/ui/account/AccountFragment.java
@@ -1,8 +1,13 @@
 package edu.uw.tcss450.messaging_final_project.ui.account;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +15,9 @@ import android.view.ViewGroup;
 
 import edu.uw.tcss450.messaging_final_project.MainActivity;
 import edu.uw.tcss450.messaging_final_project.R;
+import edu.uw.tcss450.messaging_final_project.databinding.FragmentAccountBinding;
+import edu.uw.tcss450.messaging_final_project.model.PushyTokenViewModel;
+import edu.uw.tcss450.messaging_final_project.model.UserInfoViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +29,35 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+        FragmentAccountBinding binding = FragmentAccountBinding.bind(getView());
+
+        binding.buttonSignOut.setOnClickListener(button ->{
+            signOut();
+        });
+
+    }
+
+
+    private void signOut() {
+        SharedPreferences prefs =
+                getActivity().getSharedPreferences(
+                        getString(R.string.keys_shared_prefs),
+                        Context.MODE_PRIVATE);
+        prefs.edit().remove(getString(R.string.keys_prefs_jwt)).apply();
+        PushyTokenViewModel model = new ViewModelProvider(getActivity())
+                .get(PushyTokenViewModel.class);
+        //when we hear back from the web service quit
+        model.addResponseObserver(getActivity(), result -> getActivity().finishAndRemoveTask());
+        model.deleteTokenFromWebservice(
+                new ViewModelProvider(getActivity())
+                        .get(UserInfoViewModel.class)
+                        .getmJwt()
+        );
     }
 
 }
